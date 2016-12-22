586872933976b85e0037dcae67cb589c8209fc89
Anger Jérémy <angerj.dev@gmail.com>
2016-12-22 14:14:46 +0100

    window: set the background to black

--- shaders.cpp
+++ shaders.cpp
@@ -79,8 +79,15 @@ static std::string mainFragment = scalemap + S(
 
     void main()
     {
+        vec4 border = vec4(0.0, 0.0, 0.0, 1.0);
         vec4 p = texture2D(texture, gl_TexCoord[0].xy);
-        gl_FragColor = vec4(tonemap(scalemap(p)), 1.0);
+        if (gl_TexCoord[0].x < 0.0 || gl_TexCoord[0].x > 1.0) {
+            gl_FragColor = border;
+        } else if (gl_TexCoord[0].y < 0.0 || gl_TexCoord[0].y > 1.0) {
+            gl_FragColor = border;
+        } else {
+            gl_FragColor = vec4(tonemap(scalemap(p)), 1.0);
+        }
     }
 );
 
