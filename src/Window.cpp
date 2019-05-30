0f02ecc4430a7351ae3df387a949624b62f0c0aa
Anger Jérémy <angerj.dev@gmail.com>
2019-05-30 14:44:53 +0200

    window: drag the image even when not hovered

--- src/Window.cpp
+++ src/Window.cpp
@@ -316,15 +316,15 @@ void Window::displaySequence(Sequence& seq)
         }
 
         bool dragging = ImGui::IsMouseDown(0) && (delta.x || delta.y);
-        if (ImGui::IsWindowHovered() || (dragging && !ImGui::IsAnyItemActive())) {
-            if (!ImGui::IsMouseClicked(0) && dragging) {
-                ImVec2 pos = view->window2image(ImVec2(0, 0), displayarea.getCurrentSize(), winSize, factor);
-                ImVec2 pos2 = view->window2image(delta, displayarea.getCurrentSize(), winSize, factor);
-                ImVec2 diff = pos - pos2;
-                view->center += diff / displayarea.getCurrentSize();
-                gShowView = MAX_SHOWVIEW;
-            }
+        if (!ImGui::IsMouseClicked(0) && dragging) {
+            ImVec2 pos = view->window2image(ImVec2(0, 0), displayarea.getCurrentSize(), winSize, factor);
+            ImVec2 pos2 = view->window2image(delta, displayarea.getCurrentSize(), winSize, factor);
+            ImVec2 diff = pos - pos2;
+            view->center += diff / displayarea.getCurrentSize();
+            gShowView = MAX_SHOWVIEW;
+        }
 
+        if (ImGui::IsWindowHovered() || (dragging && !ImGui::IsAnyItemActive())) {
             if (ImGui::IsMouseClicked(1)) {
                 gSelecting = true;
 
