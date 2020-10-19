5017031634f7fda49918e0e67defcb84877c4f3f
Anger Jérémy <angerj.dev@gmail.com>
2020-10-19 21:53:53 +0200

    gdal: fix loading of complex images

--- src/Image.cpp
+++ src/Image.cpp
@@ -93,10 +93,11 @@ float Image::getBandsMax(BandIndices bands) const
 
 class GDALChunkProvider : public ChunkProvider {
     GDALDataset* g;
+    bool iscomplex;
 
 public:
 
-    GDALChunkProvider(GDALDataset* g) : g(g) {
+    GDALChunkProvider(GDALDataset* g, bool iscomplex) : g(g), iscomplex(iscomplex) {
     }
 
     virtual ~GDALChunkProvider() {
@@ -108,17 +109,42 @@ public:
         size_t y = cr.cy * CHUNK_SIZE;
         if (image->getBand(cr.bandidx)->getChunk(x, y))
             return;
-        std::shared_ptr<Chunk> ck = std::make_shared<Chunk>();
-        ck->w = std::min(CHUNK_SIZE, image->w - x);
-        ck->h = std::min(CHUNK_SIZE, image->h - y);
 
-        GDALRasterBand* band = g->GetRasterBand(cr.bandidx + 1);
-        CPLErr err = band->RasterIO(GF_Read, x, y, ck->w, ck->h,
-                                    &ck->pixels[0], ck->w, ck->h, GDT_Float32, 0, 0);
-        if (err != CE_None) {
-            std::cout << " err:" + std::to_string(err) << std::endl;
+        if (!iscomplex) {
+            std::shared_ptr<Chunk> ck = std::make_shared<Chunk>();
+            ck->w = std::min(CHUNK_SIZE, image->w - x);
+            ck->h = std::min(CHUNK_SIZE, image->h - y);
+
+            GDALRasterBand* band = g->GetRasterBand(cr.bandidx + 1);
+            CPLErr err = band->RasterIO(GF_Read, x, y, ck->w, ck->h,
+                                        &ck->pixels[0], ck->w, ck->h, GDT_Float32, 0, 0);
+            if (err != CE_None) {
+                std::cout << " err:" + std::to_string(err) << std::endl;
+            }
+            image->getBand(cr.bandidx)->setChunk(x, y, ck);
+        } else {
+            // load both 'channels'
+            assert(cr.bandidx == 0);
+            std::shared_ptr<Chunk> ck1 = std::make_shared<Chunk>();
+            std::shared_ptr<Chunk> ck2 = std::make_shared<Chunk>();
+            ck2->w = ck1->w = std::min(CHUNK_SIZE, image->w - x);
+            ck2->h = ck1->h = std::min(CHUNK_SIZE, image->h - y);
+
+            std::vector<float> pixels(ck1->w * ck1->h * 2);
+
+            GDALRasterBand* band = g->GetRasterBand(1);
+            CPLErr err = band->RasterIO(GF_Read, x, y, ck1->w, ck1->h,
+                                        &pixels[0], ck1->w, ck1->h, GDT_CFloat32, 0, 0);
+            for (int i = 0; i < ck1->w * ck1->h; i++) {
+                ck1->pixels[i] = pixels[i*2+0];
+                ck2->pixels[i] = pixels[i*2+1];
+            }
+            if (err != CE_None) {
+                std::cout << " err:" + std::to_string(err) << std::endl;
+            }
+            image->getBand(0)->setChunk(x, y, ck1);
+            image->getBand(1)->setChunk(x, y, ck2);
         }
-        image->getBand(cr.bandidx)->setChunk(x, y, ck);
     }
 };
 
@@ -132,8 +158,17 @@ std::shared_ptr<Image> create_image_from_filename(const std::string& filename)
     int w = g->GetRasterXSize();
     int h = g->GetRasterYSize();
     int d = g->GetRasterCount();
+    bool iscomplex = false;
+    if (d == 1) {
+        GDALRasterBand* band = g->GetRasterBand(1);
+        GDALDataType type = band->GetRasterDataType();
+        if (GDALDataTypeIsComplex(type)) {
+            d = 2;
+            iscomplex = true;
+        }
+    }
     std::shared_ptr<Image> image = std::make_shared<Image>(w, h, d);
-    image->chunkProvider = std::make_shared<GDALChunkProvider>(g);
+    image->chunkProvider = std::make_shared<GDALChunkProvider>(g, iscomplex);
     return image;
 }
 
