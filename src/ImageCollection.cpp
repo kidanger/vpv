kidanger/vpv

commit 9b0be698c0c984f870b04452e1bbd7f22fb61f25

gdal: fix loading of complex images

src/ImageProvider.cpp
@@ -42,7 +42,17 @@ void GDALFileImageProvider::progress()
     int w = g->GetRasterXSize();
     int h = g->GetRasterYSize();
     int d = g->GetRasterCount();
-    float* pixels = (float*) malloc(sizeof(float) * w * h * d);
+    int tf = 1;
+    GDALDataType asktype = GDT_Float32;
+    if (d == 1) {
+        GDALRasterBand* band = g->GetRasterBand(1);
+        GDALDataType type = band->GetRasterDataType();
+        if (GDALDataTypeIsComplex(type)) {
+            asktype = GDT_CFloat32;
+            tf = 2;
+        }
+    }
+    float* pixels = (float*) malloc(sizeof(float) * w * h * d * tf);
     GDALRasterIOExtraArg args;
     INIT_RASTERIO_EXTRA_ARG(args);
     args.pfnProgress = [](double d, const char*, void* data){
@@ -50,9 +60,10 @@ void GDALFileImageProvider::progress()
         return 1;
     };
     args.pProgressData = this;
-    CPLErr err = g->RasterIO(GF_Read, 0, 0, w, h, pixels, w, h, GDT_Float32, d,
-                             NULL, sizeof(float)*d, sizeof(float)*w*d, sizeof(float),
+    CPLErr err = g->RasterIO(GF_Read, 0, 0, w, h, pixels, w, h, asktype, d,
+                             NULL, sizeof(float)*tf, sizeof(float)*w*d*tf, sizeof(float)*tf,
                              &args);
+    d *= tf;
     GDALClose(g);
 
     if (err != CE_None) {

commit 39c0babb4a4bb311738b0c1876b773dfea9e133b

gdal: fix loading of color images
bug introduced by 9b0be698c0c984f870b04452e1bbd7f22fb61f25

src/ImageCollection.cpp
@@ -218,7 +218,7 @@ class NumpyVideoImageCollection : public VideoImageCollection {
         FILE* file = fopen(filename.c_str(), "r");
         if (!npy_read_header(file, &ni)) {
             fprintf(stderr, "[npy] error while loading header\n");
-            exit(1);
+            //exit(1);
         }
         fclose(file);
 

src/ImageProvider.cpp
@@ -61,7 +61,7 @@ void GDALFileImageProvider::progress()
     };
     args.pProgressData = this;
     CPLErr err = g->RasterIO(GF_Read, 0, 0, w, h, pixels, w, h, asktype, d,
-                             NULL, sizeof(float)*tf, sizeof(float)*w*d*tf, sizeof(float)*tf,
+                             NULL, sizeof(float)*d*tf, sizeof(float)*w*d*tf, sizeof(float)*tf,
                              &args);
     d *= tf;
     GDALClose(g);