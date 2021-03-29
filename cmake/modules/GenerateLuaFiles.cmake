140b7b3ce4bec92a64652f95997d2c32b4b498eb
Ronny Chevalier <ronny@chevalier.io>
2021-03-29 13:18:30 +0200

    GenerateLuaFiles: generate a C++ file instead of a C one
    It also fixes a warning related to the size of a string on the stack.
    Now we store the strings in the data section rather than the stack.

--- cmake/modules/GenerateLuaFiles.cmake
+++ cmake/modules/GenerateLuaFiles.cmake
@@ -2,18 +2,22 @@
 # the function load_luafiles runs those codes and return 1 if success
 set(LUAFILES_LIST "vpvrc")
 
-set(LUAFILES_OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/luafiles.c)
+set(LUAFILES_OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/luafiles.cpp)
 
-# generate luafiles.c
+# generate luafiles.cpp
 file(WRITE ${LUAFILES_OUTPUT} "
-#include <stdio.h>
-#include <string.h>
-#include <lua.h>
-#include <lauxlib.h>
+#include <iostream>
+#include <string>
+#include <map>
+
+#include <lua.hpp>
+
+int load_luafiles(lua_State* L);
 
 int load_luafiles(lua_State* L)
 {
-    (void) L;")
+    static const std::map<std::string, std::string> luafiles = {
+")
 foreach (luacode ${LUAFILES_LIST})
     file(READ ${luacode} FILE_CONTENT)
     get_filename_component(luacode_short ${luacode} NAME)
@@ -27,20 +31,25 @@ foreach (luacode ${LUAFILES_LIST})
     string(REGEX REPLACE "\n" "\\\\n\\\\\n" FILE_CONTENT "${FILE_CONTENT}")
     file(APPEND ${LUAFILES_OUTPUT} "
     {
-        const char* file = \"${luacode_short}\";
-        const char* code = \"${FILE_CONTENT}\";
-        if (luaL_loadbuffer(L, code, strlen(code), file)) {
-            fprintf(stderr, \"%s\", lua_tostring(L, -1));
-            return 0;
-        }
-        if (lua_pcall(L, 0, 0, 0)) {
-            fprintf(stderr, \"%s\\n\", lua_tostring(L, -1));
-            return 0;
-        }
+        \"${luacode_short}\",
+        \"${FILE_CONTENT}\"
     }
 ")
 endforeach (luacode)
 file(APPEND ${LUAFILES_OUTPUT} "
+    };
+    for (const auto &kv : luafiles) {
+        const auto &file = kv.first;
+        const auto &code = kv.second;
+        if (::luaL_loadbuffer(L, code.c_str(), code.size(), file.c_str())) {
+            std::cerr << lua_tostring(L, -1) << std::endl;
+            return 0;
+        }
+        if (::lua_pcall(L, 0, 0, 0)) {
+            std::cerr << lua_tostring(L, -1) << std::endl;
+            return 0;
+        }
+    }
     return 1;
 }
 ")
