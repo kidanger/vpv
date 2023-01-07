a887765825c479229d744220ad259d0e7d9ad6ae
Anger Jérémy <angerj.dev@gmail.com>
2023-01-07 20:18:02 +0100

    v0.8.1

--- flake.nix
+++ flake.nix
@@ -6,17 +6,20 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = nixpkgs.legacyPackages.${system};
         rustPlatform = pkgs.rustPlatform;
-      in
-      {
+      in {
         packages = rec {
           vpv = pkgs.stdenv.mkDerivation rec {
             pname = "vpv";
-            version = "0.8.0";
+            version = "0.8.1";
             src = ./.;
 
             cargoRoot = "src/fuzzy-finder";
