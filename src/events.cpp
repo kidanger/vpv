462257174f452e7ef6bc13a0cd6dfd427f7babe2
Anger Jérémy <angerj.dev@gmail.com>
2023-09-07 09:17:29 +0200

    events: use keycodes instead of scancodes
    this should fix issues related to keyboard layouts

--- src/events.cpp
+++ src/events.cpp
@@ -8,35 +8,38 @@
 
 int getCode(const char* name)
 {
-#define specials(n, sdl, sfml)     \
+#define specials(n, sdl)     \
     if (std::string(name) == #n) { \
-        return SDL_SCANCODE_##sdl; \
+        return SDLK_##sdl & ~SDLK_SCANCODE_MASK; \
     }
 
-    specials(left, LEFT, Left);
-    specials(right, RIGHT, Right);
-    specials(up, UP, Up);
-    specials(down, DOWN, Down);
-    specials(F1, F1, F1);
-    specials(F2, F2, F2);
-    specials(F3, F3, F3);
-    specials(F4, F4, F4);
-    specials(F5, F5, F5);
-    specials(F6, F6, F6);
-    specials(F7, F7, F7);
-    specials(F8, F8, F8);
-    specials(F9, F9, F9);
-    specials(F10, F10, F10);
-    specials(F11, F11, F11);
-    specials(F12, F12, F12);
+    specials(left, LEFT);
+    specials(right, RIGHT);
+    specials(up, UP);
+    specials(down, DOWN);
+    specials(F1, F1);
+    specials(F2, F2);
+    specials(F3, F3);
+    specials(F4, F4);
+    specials(F5, F5);
+    specials(F6, F6);
+    specials(F7, F7);
+    specials(F8, F8);
+    specials(F9, F9);
+    specials(F10, F10);
+    specials(F11, F11);
+    specials(F12, F12);
 
 #undef specials
 
     switch (*name) {
     default: {
         SDL_Keycode key = SDL_GetKeyFromName(name);
-        if (key != SDLK_UNKNOWN)
-            return SDL_GetScancodeFromKey(key);
+        if (key != SDLK_UNKNOWN) {
+            key &= ~SDLK_SCANCODE_MASK;
+            if (key < 512)
+                return key;
+        }
     }
     }
     printf("unknown key '%s'\n", name);
